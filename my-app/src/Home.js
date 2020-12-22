2d16ae9e0f268523b142ade1e3f1c0b89909adb5
code-250 <richmunye@gmail.com>
2020-12-22 02:35:02 +0200

    linking pages in my brand

---
+++ my-app/src/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Card from './UI/components/Card';
+import HelloSection from './UI/components/HelloSection';
+
+function Home(){
+    return(
+        <div>
+            <HelloSection/>
+            <Card/>
+       
+    )
+        </div>
+        
+    )
+}
+export default Home;
--- my-app/src/UI/components/Pages/blogs.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import '../../../App.css';
-
-export default function blogs(){
-    return<h1 className='blogs'>BLOGS</h1>
-}
