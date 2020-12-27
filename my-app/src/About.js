bf418d02409d555ec4e2c69a1f60a774c3a9cc8c
code-250 <richmunye@gmail.com>
2020-12-27 22:29:09 +0200

    starting to work oa the about page

--- my-app/src/About.js
+++ my-app/src/About.js
@@ -1,11 +1,35 @@
 import  React from 'react'
 import './App.css';
+import './About.css';
 import Footer from './UI/components/Footer';
 
 function About(){
     return(
         <div className='App'>
             <h1>Hello About</h1>
+            <div className='about-container'>
+                <div className='about-wrapper'>
+                    <p>who is this guy?</p>
+                    <p>what does he do ?</p> 
+                    <p>what do we need to know about him?</p>
+                </div>
+            </div>    
+                    <div>
+                        <p>
+                            I am a beginer fullstack developer 
+                            who is dedicated into helpng people 
+                            in other words users to have the most
+                            responsive websites and web apps.
+                            Below are different services that we offer 
+                            and the expertise.
+                        </p>
+                    </div>
+                    {/* services that we offer. */}
+
+                    {/* expertise */}
+
+                    {/* what our clients talk about us */}
+                    
             <Footer/>
         </div>
     )
--- my-app/src/UI/components/Footer.js
+++ my-app/src/UI/components/Footer.js
@@ -4,128 +4,107 @@ import { Button } from './Button';
 import {Link} from 'react-router-dom';
 import './Footer.css';
 
+
 function Footer() {
     return (
-        <div className='footer-container'>
-            <section className='footer-subscription'>
-                <h1 className='footer-subscription-heading'>
-                    Have a new project to work on 
-                </h1>
-                <p>
-                    you can unsubscribe at any time.
-                </p>
-                <div className="input-areas">
-                    <form>
-                        <input 
-                         type='email'
-                         placeholder='your email'
-                         className='footer-input'
-                         />
-                         <Button buttonStyle='btn--outline'className='Subscribe'>Subscribtion</Button>
-                    </form>
-                </div>
-            </section>
-            <section className='footer-contact'>
-                <h1>
-                    CONTACT US
-                </h1>
-                <div className='contact-container'>
-                    <div className='contact-wrapper'>
-                        <form classNane='form'>
-                            <input type='text'
-                            placeholder="your Full Name"
-                            className='contact-input'/>
-                            <input type='email'
-                            placeholder="your email"
-                            className='contact-input'/>
+        <>
+            <div className='footer-container'>
+                <section className='footer-subscription'>
+                    <h1 className='footer-subscription-heading'>
+                        Have a new project to work on 
+                    </h1>
+                    <p>
+                        you can unsubscribe at any time.
+                    </p>
+                    <div className="input-areas">
+                        <form>
+                            <input 
+                            type='email'
+                            placeholder='your email'
+                            className='footer-input'
+                            />
+                            <Button buttonStyle='btn--outline'className='Subscribe'>Subscribtion</Button>
                         </form>
                     </div>
-                    <div className='message'>
-                         <input type='text'
-                        placeholder="Send your message here"
-                        className='contact-message'/>
-                        <Button buttonStyle='btn--outline'
-                        className='Subscribe'
-                        >Send</Button>
+                </section>
+                <section className='footer-contact'>
+                    <h1>
+                        CONTACT US
+                    </h1>
+                    <div className='contact-container'>
+                        <div className='contact-wrapper'>
+                            <form classNane='form'>
+                                <input type='text'
+                                placeholder="your Full Name"
+                                className='contact-input'/>
+                                <input type='email'
+                                placeholder="your email"
+                                className='contact-input'/>
+                            </form>
+                        </div>
+                        <div className='message'>
+                            <input type='text'
+                            placeholder="Send your message here"
+                            className='contact-message'/>
+                            <Button buttonStyle='btn--outline'
+                            className='Subscribe'
+                            >Send</Button>
+                            
+                        </div>
+                    
+                    </div>
+                    
+                    
+                </section>
+                <div className='footer-links'>
+                    <div className='footer-link-wrapper'>
+                    <div className='footer-link-items'>
+                            <h2>Projects</h2>
+                            <Link to='/'>Want to try this </Link>
+                            <Link to='/'>What are we capable of?</Link>
+                            <Link to='/'>Work with us</Link>
+                            <Link to='/'>Who is </Link>
+                        </div>
+                        <div className='footer-link-items'>
+                            <h2>About Us</h2>
+                            <Link to='/About'>Who is this guy?</Link>
+                            <Link to='/'>What are we capable of?</Link>
+                            <Link to='/'>Work with us</Link>
+                            <Link to='/'>Who is </Link>
+                        </div>
                         
                     </div>
-                   
-                </div>
-                
-                
-            </section>
-            <div className='footer-links'>
-                <div className='footer-link-wrapper'>
-                <div className='footer-link-items'>
-                        <h2>Projects</h2>
-                        <Link to='/'>Want to try this </Link>
-                        <Link to='/'>What are we capable of?</Link>
-                        <Link to='/'>Work with us</Link>
-                        <Link to='/'>Who is </Link>
+                    <div className='footer-link-wrapper'>
+                        
+                        <div className='footer-link-items'>
+                            <h2>Contact Us</h2>
+                            <Link to='/Projects'>Contact</Link>
+                            <Link to='/'>Drop an idea and inquiries</Link>
+                            <Link to='/'>Work with us</Link>
+                            <Link to='/'>Who is </Link>
+                        </div>
+                        <div className='footer-link-items'>
+                            <h2>Social Media</h2>
+                            <Link to='/'>Facebook</Link>
+                            <Link to='/'>Twitter</Link>
+                            <Link to='/'>Instagram</Link>
+                            <Link to='/'>Linked In</Link>
+                        </div>
                     </div>
-                    <div className='footer-link-items'>
-                        <h2>About Us</h2>
-                        <Link to='/About'>Who is this guy?</Link>
-                        <Link to='/'>What are we capable of?</Link>
-                        <Link to='/'>Work with us</Link>
-                        <Link to='/'>Who is </Link>
+                </div>
+                <section className='social-media'>
+                    <div className='social-media-wrap'>
+                    <small className='website-rights'>no copyright reserved</small>
+                        <div className='social-icons'>
+                            
+                        </div>
+                    
                     </div>
                     
-                </div>
-                <div className='footer-link-wrapper'>
-                    
-                    <div className='footer-link-items'>
-                        <h2>Contact Us</h2>
-                        <Link to='/Projects'>Contact</Link>
-                        <Link to='/'>Drop an idea and inquiries</Link>
-                        <Link to='/'>Work with us</Link>
-                        <Link to='/'>Who is </Link>
-                    </div>
-                    <div className='footer-link-items'>
-                        <h2>Social Media</h2>
-                        <Link to='/'>Facebook</Link>
-                        <Link to='/'>Twitter</Link>
-                        <Link to='/'>Instagram</Link>
-                        <Link to='/'>Linked In</Link>
-                    </div>
-                </div>
+                </section>
+                
             </div>
-            <section className='social-media'>
-                <div className='social-media-wrap'>
-                <small className='website-rights'>no copyright reserved</small>
-                    <div className='social-icons'>
-                        <Link to='/'
-                        target-label='_blank'
-                        arial-label='Facebook'
-                        className='social-icon-link facebook'
-                        
-                        >
-                            <i className='fab-fa-facebook-f'>
-                                <faFacebook color="white"/>
-                            </i>
-                        </Link>
-                        <Link to='/'
-                        target-label='_blank'
-                        arial-label='Twitter'
-                        className='social-icon-link twitter'
-                        >
-                            <i className='fab-fa-twitter'></i>
-                        </Link>
-                        <Link to='/'
-                        target-label='_blank'
-                        arial-label='LinkedIn'
-                        className='social-icon-link linkedin'
-                        >
-                            <i className='fab-fa-linkedin-f'></i>
-                        </Link>
-                        
-                    </div>
-                   
-                </div>
-                
-            </section>
-            
-        </div>
+        </>
     )
 }
 
